61fa6bdbdd1b345ceec446aea7e00dbf558fc2fb
Evan Borgstrom <evan@fatbox.ca>
2013-11-01 22:42:11 -0400

    Little bit more...

--- lib/deployzilla/__init__.py
+++ lib/deployzilla/__init__.py
@@ -6,10 +6,22 @@ class Deployzilla(object):
     def main(self):
         # check if we have stdin data
         if not select.select([sys.stdin,],[],[],0.0)[0]:
-            print "ERROR: You need to invoke Deployzilla from a post-receive hook (no stdin)"
+            print "ERROR: You need to invoke Deployzilla from a post-receive" \
+                  "hook (no stdin)"
             return
 
         # read in our git data
-        old, new, ref = str(sys.stdin.read()).split()
+        try:
+            input_data = sys.stdin.read()
+            old, new, ref = input_data.split()
+        except ValueError:
+            print "ERROR: Oops. Either GIT **REALLY** messed up or someone is" \
+                  "invoking Deployzilla by hand..."
+            return
 
-        print ref
+        ref = ref.lstrip("refs/heads/")
+        self.check_deployment(ref)
+
+    def check_deployment(self, ref):
+        "Use git to check if we have a deploy.yaml config file"
+        pass
