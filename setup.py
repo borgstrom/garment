9e129d5608dcf6f56f5c7f8d19b41762b463a9d2
Evan Borgstrom <evan@fatbox.ca>
2013-12-29 20:38:01 -0500

    Use versioneer

--- setup.py
+++ setup.py
@@ -2,15 +2,23 @@
 
 from setuptools import setup
 
+import versioneer
+
+versioneer.versionfile_source = 'garment/_version.py'
+versioneer.versionfile_build = 'garment/_version.py'
+versioneer.tag_prefix = '' # versions are like '1.0.4'
+versioneer.parentdir_prefix = 'garment-' # dir names are like 'garment-1.0.4'
+
 setup(name='garment',
       packages=['garment'],
       include_package_data=True,
-      version='0.1',
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
       license="Apache License, Version 2.0",
       description='A collection of fabric tasks that roll up into a single deploy function. The whole process is coordinated through a single deployment configuration file named deploy.conf',
       long_description=open('README.rst').read(),
       author='Evan Borgstrom',
-      author_email='evan@fatbox.ca',
+      author_email='evan@borgstrom.ca',
       url='https://github.com/fatbox/garment',
       classifiers=[
           'Development Status :: 3 - Alpha',
